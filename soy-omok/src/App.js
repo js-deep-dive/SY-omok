fa5f4b0ceea5d9fb17364dd4b6a8c15eb9ab45ab
SoYoung210 <ethdud1@gmail.com>
2018-09-30 22:53:04 +0900

    [#1] feat : 바둑판 셋팅.

--- soy-omok/src/App.js
+++ soy-omok/src/App.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      size: 16,
+      borad: Array(16 * 16).fill(null) // create 16 by 16 board.
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +18,23 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">Soyoung's first react project</p>
+
+        <div className="Omok">
+          {Array(this.state.size)
+            .fill(null)
+            .map(row => {
+              return (
+                <div className="row">
+                  {Array(this.state.size)
+                    .fill(null)
+                    .map(col => {
+                      return <div className="col" />;
+                    })}
+                </div>
+              );
+            })}
+        </div>
       </div>
     );
   }
